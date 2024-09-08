43406663b52c7280d4c0f815cfd0e00bdc733232
chau-nm <chaunm.146.2001@gmail.com>
2024-09-08 22:13:22 +0700

    security day 1

--- src/main/java/com/app/spring_journey/config/SecurityConfig.java
+++ src/main/java/com/app/spring_journey/config/SecurityConfig.java
@@ -1,5 +1,63 @@
 package com.app.spring_journey.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
   
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf((csrf) -> csrf.disable())
+      .authorizeHttpRequests((authorize) -> authorize
+        .requestMatchers(
+          "/api/auth/login"
+          )
+        .permitAll()
+        .anyRequest().authenticated()
+      );
+    return http.build();
+  }
+
+  @Bean
+	public AuthenticationManager authenticationManager(
+			UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+		return new ProviderManager(authenticationProvider);
+	}
+
+  @Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.builder()
+			.username("user")
+			.password(passwordEncoder().encode("password"))
+			.roles("USER")
+			.build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
--- src/main/java/com/app/spring_journey/controller/AuthController.java
+++ src/main/java/com/app/spring_journey/controller/AuthController.java
@@ -1,5 +1,10 @@
 package com.app.spring_journey.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +17,20 @@ import com.app.spring_journey.model.LoginPayload;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
   @PostMapping("/login")
   public boolean login(@RequestBody LoginPayload loginPayload) {
-    if (loginPayload.getUsername().equals("admin") && loginPayload.getPassword().equals("admin")) {
+    System.out.println("Start login");
+    try {
+      Authentication authenticationRequest = 
+        UsernamePasswordAuthenticationToken.unauthenticated(loginPayload.getUsername(), loginPayload.getPassword());
+      authenticationManager.authenticate(authenticationRequest);
       return true;
+    } catch (AuthenticationException e) {
+      return false;
     }
-    return false;
   }
 
   @GetMapping("/hello")
