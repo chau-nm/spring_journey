46cc3645b0d481f42de441d4ef4d59fda728f7b2
chau-nm <chaunm.146.2001@gmail.com>
2024-09-08 22:14:57 +0700

    remove redundant import

--- src/main/java/com/app/spring_journey/config/SecurityConfig.java
+++ src/main/java/com/app/spring_journey/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
